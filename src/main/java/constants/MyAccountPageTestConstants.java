532dc6077086b9b997cd4816690dfcf877820635
cato38 <90105044+cato38@users.noreply.github.com>
2021-09-05 13:02:55 +0300

    Update MyAccountPageTestConstants.java

--- src/main/java/constants/MyAccountPageTestConstants.java
+++ src/main/java/constants/MyAccountPageTestConstants.java
@@ -6,7 +6,7 @@ public class MyAccountPageTestConstants {
     public static final String PASSWORD = "Mervehus*34";
     public static final String FIRSTNAME = "Cagatay";
     public static final String SURNAME = "Tomu";
-    public static String NEWPASSWD = "Cato!147741";
+    public static String NEWPASSWD = "Mervehus*34";
     public static final String MAIL_ADDRESS_DEMO = "cagataytomu"+ RandomStringUtils.randomNumeric(2)+"@outlook.com";
     public static final String MAIL_ADDRESS = "cagataytomu@outlook.com";
 }
