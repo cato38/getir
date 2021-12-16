5ebedc88c19aadf7bbc3d114df4a3de0c31d3fe4
cato38 <90105044+cato38@users.noreply.github.com>
2021-12-16 12:19:29 +0300

    Update MyAccountPageTestConstants.java

--- src/main/java/constants/MyAccountPageTestConstants.java
+++ src/main/java/constants/MyAccountPageTestConstants.java
@@ -3,10 +3,10 @@ package constants;
 import org.apache.commons.lang3.RandomStringUtils;
 
 public class MyAccountPageTestConstants {
-    public static final String PASSWORD = "Mervehus*34";
+    public static final String PASSWORD = "changeit";
     public static final String FIRSTNAME = "Cagatay";
     public static final String SURNAME = "Tomu";
-    public static String NEWPASSWD = "Mervehus*34";
+    public static String NEWPASSWD = "changeit";
     public static final String MAIL_ADDRESS_DEMO = "cagataytomu"+ RandomStringUtils.randomNumeric(2)+"@outlook.com";
-    public static final String MAIL_ADDRESS = "cagataytomu@outlook.com";
+    public static final String MAIL_ADDRESS = "cagatayzor@outlook.com";
 }
